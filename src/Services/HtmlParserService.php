3220d1a674b64bf8d1af37d300de3cb795f1e36b
max <htugjxnf@yandex.ru>
2022-08-24 18:03:08 +1000

    Добавляю Парсер

---
+++ src/Services/HtmlParserService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use voku\helper\HtmlDomParser;
+
+class HtmlParserService
+{
+    const PURCHASES_BLOCK_SELECTOR = '.purchases.block';
+    const PURCHASES_WRAPPER_BLOCK_SELECTOR = '.purchases-wrapper.purchases-items-wrapper .purchase-block';
+    const PURCHASE_PROPERTIES_SELECTOR = '.properties .name a';
+    const PURCHASE_PICTURE_SELECTOR = '.picture a img';
+
+    public static function parse(string $html): array
+    {
+        $purchaseTypes = [];
+        $html = HtmlDomParser::str_get_html($html);
+
+        $purchasesBlocks = $html->findMulti(self::PURCHASES_BLOCK_SELECTOR);
+        foreach ($purchasesBlocks as $purchasesBlock) {
+            $purchaseType = new \stdClass();
+            $purchaseType->name = trim($purchasesBlock->id);
+            // Не рассматриваем блок без типа (id) покупки
+            if (!$purchaseType->name) continue;
+            $purchaseType->purchases = [];
+
+            $purchasesWrapperBlocks = $purchasesBlock->findMulti(self::PURCHASES_WRAPPER_BLOCK_SELECTOR);
+            foreach ($purchasesWrapperBlocks as $purchasesWrapperBlock) {
+                $purchaseProperties = $purchasesWrapperBlock->findOne(self::PURCHASE_PROPERTIES_SELECTOR);
+
+                $purchase = new \stdClass();
+                $purchase->id = trim($purchaseProperties->getAttribute('data-item-id'));;
+                $purchase->name = trim($purchaseProperties->innertext);
+                $purchase->url = trim($purchaseProperties->href);
+                $purchase->photo = trim($purchasesWrapperBlock->findOne(self::PURCHASE_PICTURE_SELECTOR)->src);
+
+                // Не берем покупку без всех необходимых данных
+                foreach ($purchase as $field) {
+                    if (empty($field)) continue;
+                }
+
+                $purchaseType->purchases[] = $purchase;
+            }
+            $purchaseTypes[] = $purchaseType;
+        }
+        return $purchaseTypes;
+    }
+}
